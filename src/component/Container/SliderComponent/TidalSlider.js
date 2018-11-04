cfec96ec2f6f6984c32cffe496804d487f0a1987
Amman Vedi <ammanvedi@Ammans-MacBook-Pro.local>
2018-11-04 17:27:16 +0000

    create modelling functions

--- src/component/Container/SliderComponent/TidalSlider.js
+++ src/component/Container/SliderComponent/TidalSlider.js
@@ -1,10 +1,65 @@
 // @flow
 
 import { tidalDataFetch } from '../../../store/Action/TidalDataAction';
-import type { TidalFavoritesData, TidalFavoriteWrapper } from '../../../store/Type';
-import type { AlbumList, AlbumModel } from '../../Type';
+import type { TidalFavoritesData, TidalFavoriteWrapper, TidalFavoriteItem, TidalArtist } from '../../../store/Type';
+import type { AlbumList, AlbumModel, SongModel, ArtistModel } from '../../Type';
 import { AlbumItem } from '../AlbumItem/AlbumItem';
 import { createSliderComponent } from './SliderComponent';
+
+type ImageSize = 80 | 160 | 320 | 640 | 1280;
+
+const getTidalImageUrl = ( imageHash: string, size: ImageSize = 320 ): string => {
+    const uuidPath: string = imageHash.split( '-' ).join( '/' );
+    return `https://resources.tidal.com/images/${ uuidPath }/${ size.toString() }x${ size.toString() }.jpg`
+}
+
+const getAlbumModel = ( tidalInfo: TidalFavoriteItem ): AlbumModel => ( {
+    id: tidalInfo.album.id.toString(),
+    title: tidalInfo.album.title,
+    imageUrl: getTidalImageUrl( tidalInfo.album.cover ),
+    songs: [],
+    artists: [ getArtistModel( tidalInfo.artist ) ],
+    url: tidalInfo.id.toString()
+} )
+
+const getSongModel = ( tidalInfo: TidalFavoriteItem ): SongModel => ( {
+    id: tidalInfo.id.toString(),
+    title: tidalInfo.title,
+    artists: tidalInfo.artists.map( artist => getArtistModel( artist ) ),
+    url: tidalInfo.id.toString()
+} );
+
+const getArtistModel = ( artist: TidalArtist ): ArtistModel => ( {
+    id: artist.id.toString(),
+    name: artist.name
+} );
+
+type IntermediateAlbumMapping = Map<number, AlbumModel>
+
+const getAlbumList = ( tidalData: TidalFavoritesData ): AlbumList => {
+
+    if ( !tidalData.items ) {
+        return [];
+    }
+
+    const albumMap = tidalData.items.reduce( ( mapping: IntermediateAlbumMapping, wrapper: TidalFavoriteWrapper ): IntermediateAlbumMapping => {
+        const tidalItem: TidalFavoriteItem = wrapper.item;
+
+        if ( !mapping.has( tidalItem.album.id ) ) {
+            mapping.set( tidalItem.album.id, getAlbumModel( tidalItem ) );
+        }
+
+        const val = mapping.get( tidalItem.album.id );
+        if( val ) {
+            val.songs.push( getSongModel( tidalItem ) )
+        }
+
+        return mapping;
+    }, new Map() );
+
+    return [ ...albumMap.values() ];
+
+}
 
 const transform = ( data: TidalFavoritesData ): AlbumList => {
 
@@ -12,14 +67,7 @@ const transform = ( data: TidalFavoritesData ): AlbumList => {
         return [];
     }
 
-    return data.items.map( ( tidalItem: TidalFavoriteWrapper ): AlbumModel => ( {
-        id: tidalItem.item.id.toString(),
-        title: tidalItem.item.album.title,
-        imageUrl: tidalItem.item.album.cover,
-        songs: [],
-        artists: [],
-        url: ''
-    } ) );
+    return getAlbumList( data );
 }
 
 export const TidalSlider = createSliderComponent( 'tidalData', tidalDataFetch, transform, AlbumItem );
