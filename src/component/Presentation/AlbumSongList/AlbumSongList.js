3370882c542b9bca7b435ccee08c608188c50521
Amman Vedi <ammanvedi@Ammans-MacBook-Pro.local>
2018-11-10 19:25:03 +0000

    add play icon

--- src/component/Presentation/AlbumSongList/AlbumSongList.js
+++ src/component/Presentation/AlbumSongList/AlbumSongList.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import type { SongModel } from '../../Type';
 import styles from './AlbumSongList.scss';
+import SVG from 'react-inlinesvg';
+import Play from '../../../svg/play.svg';
 
 export type AlbumSongListProps = {
     songs: Array<SongModel>,
@@ -17,6 +19,7 @@ export const AlbumSongList = ( props: AlbumSongListProps ) => {
                     return (
                         <li key={ song.id } >
                             <a href={song.url} >
+                                <SVG src={ Play } />
                                 <span>{song.title}</span>
                                 <ul className="album-song-list__artists" >
                                     { song.artists.map( artist => <li key={artist.id} >{ artist.name }</li> ) }
