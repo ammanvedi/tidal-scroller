73088eb9595e86e9138847e0a0715db843e55c07
Amman Vedi <ammanvedi@Ammans-MacBook-Pro.local>
2018-11-10 19:20:32 +0000

    global css imports

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom';
 import { TidalSlider } from './component/Container/SliderComponent/TidalSlider';
 import { Provider } from 'react-redux';
 import store from './store/Store'
+/* eslint-disable-next-line no-unused-vars */
+import styles from './styles/Global.scss';
+/* eslint-disable-next-line no-unused-vars */
+import fonts from './styles/Global.scss';
 
 class App extends React.Component {
     render() {
