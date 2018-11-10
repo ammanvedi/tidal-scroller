542f5a3a63e759c80c9fba25147494d919d8bc37
Amman Vedi <ammanvedi@Ammans-MacBook-Pro.local>
2018-11-10 19:24:10 +0000

    add svg and time ago rendering

--- src/component/Presentation/AlbumCover/AlbumCover.js
+++ src/component/Presentation/AlbumCover/AlbumCover.js
@@ -1,10 +1,12 @@
 // @flow
 
 import React from 'react';
-
 import { LazyImage } from '../../Container/LazyImage/LazyImage';
-
+import styles from './AlbumCover.scss';
 import type { AlbumModel } from '../../Type';
+import { timeAgo } from '../../../util/TimeAgo';
+import SVGInline from "react-svg-inline"
+import Heart from '../../../svg/heart.svg';
 
 export type AlbumCoverProps = {
     className: string,
@@ -15,16 +17,30 @@ export type AlbumCoverProps = {
 
 export const AlbumCover = ( props: AlbumCoverProps ) => {
 
+    const now = new Date().getTime();
+
     return (
         <a  href={ props.album.url }
-            className={ `album-cover ${ props.className }` }
+            className={ `${ styles.album_cover } ${ styles[ props.className ] }` }
             onMouseEnter={ () => props.onMouseEnter() }
             onMouseLeave={ () => props.onMouseLeave() } >
             <LazyImage url={ props.album.imageUrl } alt={ `Album for ${ props.album.title }` } />
-            <span>{ props.album.title }</span>
-            <ul className="album-cover__artists" >
-                { props.album.artists.map( artist => <li key={ artist.id } >{ artist.name }</li> ) }
-            </ul>
+
+            <div className={ styles.album_cover__meta } >
+                <div className={ styles.album_cover__meta_top } >
+                    <SVGInline svg={ Heart } />
+                    <span className={ styles.album_cover__track_count } >
+                        { props.album.songs.length === 1 ? '1 Track' : `${ props.album.songs.length } Tracks` }
+                    </span>
+                    <span className={ styles.album_cover__timestamp }>
+                        { timeAgo( now, new Date( props.album.lastUpdated ).getTime() ) }
+                    </span>
+                </div>
+                <span className={ styles.album_cover__title } >{ props.album.title }</span>
+                <ul className={ styles.album_cover__artists } >
+                    { props.album.artists.map( artist => <li key={ artist.id } >{ artist.name }</li> ) }
+                </ul>
+            </div>
         </a>
     )
 }
