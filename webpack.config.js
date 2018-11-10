4ff4fad2ae28513de51000f850b1c4d4c3ce1444
Amman Vedi <ammanvedi@Ammans-MacBook-Pro.local>
2018-11-10 19:22:20 +0000

    webpack svg config

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
     module: {
       rules: [
         {
+            test: /\.svg$/,
+            loader: 'svg-inline-loader'
+        },
+        {
             test: /\.js$/,
             exclude: /node_modules/,
             use: [
