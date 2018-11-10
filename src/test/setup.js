7fd6a14733ef72f844daec18de4256d4f6c7b781
Amman Vedi <ammanvedi@Ammans-MacBook-Pro.local>
2018-11-10 19:22:41 +0000

    mock the intersection observer

--- src/test/setup.js
+++ src/test/setup.js
@@ -1,6 +1,26 @@
 // this file is used to setup Jest and Enzyme
-
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+
 Enzyme.configure({ adapter: new Adapter() });
+
+
+global.IntersectionObserver = class IOMock {
+    constructor( cb ) {
+        this.cb = cb;
+    }
+
+    observe( element ) {
+        this.observing = element;
+        this.cb( [
+            {
+                intersectionRatio: 1,
+                isIntersecting: true,
+                target: this.observing
+            }
+        ] )
+    }
+
+    unobserve() {}
+}
