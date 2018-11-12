50f9a0b2f59d250a9d58f6d77f7441728e42ca45
Amman Vedi <ammanvedi@Ammans-MacBook-Pro.local>
2018-11-12 17:44:43 +0000

    song list markups

--- src/component/Presentation/AlbumSongList/AlbumSongList.js
+++ src/component/Presentation/AlbumSongList/AlbumSongList.js
@@ -3,8 +3,10 @@
 import React from 'react';
 import type { SongModel } from '../../Type';
 import styles from './AlbumSongList.scss';
-import SVG from 'react-inlinesvg';
+import songStyles from './Song.scss';
+import SVGInline from "react-svg-inline"
 import Play from '../../../svg/play.svg';
+import { durationFormatter } from '../../../util/DurationFormatter';
 
 export type AlbumSongListProps = {
     songs: Array<SongModel>,
@@ -13,17 +15,26 @@ export type AlbumSongListProps = {
 
 export const AlbumSongList = ( props: AlbumSongListProps ) => {
     return (
-        <ul className={ `${ styles[ 'album-song-list' ] } ${ props.className }` } >
+        <ul className={ `${ styles[ 'album-song-list' ] } ${ styles[ props.className ] }` } >
             {
                 props.songs.map( song => {
                     return (
-                        <li key={ song.id } >
-                            <a href={song.url} >
-                                <SVG src={ Play } />
-                                <span>{song.title}</span>
-                                <ul className="album-song-list__artists" >
-                                    { song.artists.map( artist => <li key={artist.id} >{ artist.name }</li> ) }
-                                </ul>
+                        <li key={ song.id } className={ songStyles.song } >
+                            <a className={ songStyles.song__link } href={song.url} target="_blank" rel="noopener noreferrer" >
+                                <div className={ songStyles.song__header } >
+                                    <div className={ songStyles.song__play_icon } >
+                                        <SVGInline svg={ Play } />
+                                    </div>
+                                    <span className={ songStyles.song__heading } >{song.title}</span>
+                                </div>
+
+                                <div className={ songStyles.song__meta } >
+                                    <span className={ songStyles.song__meta_text } >{song.artists.map( a => a.name ).join( ', ' )}</span>
+                                    <div className={ songStyles.song__dot }></div>
+                                    <span className={ songStyles.song__meta_text } >{ durationFormatter( song.duration ) }</span>
+                                    { song.explicit ? <div className={ songStyles.song__dot }></div> : '' }
+                                    { song.explicit ? <span className={ songStyles.song__explicit } >E</span> : '' }
+                                </div>
                             </a>
                         </li>
                     )
