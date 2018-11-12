74af3fcef272115d078531b4092ab379a5a9c6aa
Amman Vedi <ammanvedi@Ammans-MacBook-Pro.local>
2018-11-12 17:44:34 +0000

    album cover markup

--- src/component/Presentation/AlbumCover/AlbumCover.js
+++ src/component/Presentation/AlbumCover/AlbumCover.js
@@ -5,6 +5,7 @@ import { LazyImage } from '../../Container/LazyImage/LazyImage';
 import styles from './AlbumCover.scss';
 import type { AlbumModel } from '../../Type';
 import { timeAgo } from '../../../util/TimeAgo';
+import { AlbumSongList } from '../AlbumSongList/AlbumSongList';
 import SVGInline from "react-svg-inline"
 import Heart from '../../../svg/heart.svg';
 
@@ -12,35 +13,41 @@ export type AlbumCoverProps = {
     className: string,
     album: AlbumModel,
     onMouseEnter: () => void,
-    onMouseLeave: () => void
+    onMouseLeave: () => void,
+    listVisible: boolean
 }
 
 export const AlbumCover = ( props: AlbumCoverProps ) => {
 
     const now = new Date().getTime();
 
+
+
     return (
-        <a  href={ props.album.url }
-            className={ `${ styles.album_cover } ${ styles[ props.className ] }` }
+        <div
+            className={ `${ styles.album_cover } ${ props.listVisible ? styles[ 'album_cover--dark-gradient' ] : '' }` }
             onMouseEnter={ () => props.onMouseEnter() }
             onMouseLeave={ () => props.onMouseLeave() } >
             <LazyImage url={ props.album.imageUrl } alt={ `Album for ${ props.album.title }` } />
 
-            <div className={ styles.album_cover__meta } >
-                <div className={ styles.album_cover__meta_top } >
-                    <SVGInline svg={ Heart } />
-                    <span className={ styles.album_cover__track_count } >
-                        { props.album.songs.length === 1 ? '1 Track' : `${ props.album.songs.length } Tracks` }
-                    </span>
-                    <span className={ styles.album_cover__timestamp }>
-                        { timeAgo( now, new Date( props.album.lastUpdated ).getTime() ) }
-                    </span>
+            <div className={ `${ styles.album_cover__meta } ${ props.listVisible ? styles[ 'album_cover__meta--show-list' ] : '' }` } >
+                <div className={ styles.album_cover__meta_wrap }>
+                    <div className={ styles.album_cover__meta_top } >
+                        <SVGInline svg={ Heart } />
+                        <span className={ styles.album_cover__track_count } >
+                            { props.album.songs.length === 1 ? '1 Track' : `${ props.album.songs.length } Tracks` }
+                        </span>
+                        <span className={ styles.album_cover__timestamp }>
+                            { timeAgo( now, new Date( props.album.lastUpdated ).getTime() ) }
+                        </span>
+                    </div>
+                    <span className={ styles.album_cover__title } >{ props.album.title }</span>
+                    <ul className={ styles.album_cover__artists } >
+                        { props.album.artists.map( artist => <li key={ artist.id } >{ artist.name }</li> ) }
+                    </ul>
                 </div>
-                <span className={ styles.album_cover__title } >{ props.album.title }</span>
-                <ul className={ styles.album_cover__artists } >
-                    { props.album.artists.map( artist => <li key={ artist.id } >{ artist.name }</li> ) }
-                </ul>
+                <AlbumSongList songs={ props.album.songs } className={ props.listVisible ? 'album-song-list--show' : '' } />
             </div>
-        </a>
+        </div>
     )
 }
